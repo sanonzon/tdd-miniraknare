1c283ba09362a43be36cee3129df4df27d18f9a5
Axel Pennsäter <pennsater@gmail.com>
2016-04-04 12:51:02 +0200

    tjosan

---
+++ src/GUI.java
@@ -0,0 +1,18 @@
+/**
+ * Created by suddan on 2016-04-04.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class GUI extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
